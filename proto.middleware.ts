f2191bb97e43d72ee0e2862f696383e0121b1b6c
jim.acero.valcarcel <jim.acero.valcarcel@gmail.com>
2024-08-23 00:57:42 +0000

    Catch decode errors

--- proto.middleware.ts
+++ proto.middleware.ts
@@ -10,8 +10,17 @@ export interface ProtoRequest extends Request<any, any, HelloRequest> {}
 interface InProtoRequest extends Request<any, any, Buffer | HelloRequest> {}
 
 const proto: RequestHandler = (req: InProtoRequest, res: ProtoResponse, next) => {
-    const helloRequest = HelloRequest.decode(req.body as Buffer);
-    req.body = helloRequest;
+    try {
+        const helloRequest = HelloRequest.decode(req.body as Buffer);
+        req.body = helloRequest;
+    } catch (error) {
+        if (error instanceof $protobuf.util.ProtocolError) {
+            return res.status(400)
+                .send('API_ERROR(decode): ' + error.message);
+        }
+        return res.status(500)
+        .send('API_ERROR(decode): ' + error.message);
+    }
 
     res.proto = (msg) => {
         try {
@@ -21,10 +30,10 @@ const proto: RequestHandler = (req: InProtoRequest, res: ProtoResponse, next) =>
         } catch (error) {
             if (error instanceof $protobuf.util.ProtocolError) {
                 return res.status(400)
-                    .send('API_ERROR: ' + error.message+typeof req.body);
+                    .send('API_ERROR(encode): ' + error.message+typeof req.body);
             }
             return res.status(500)
-            .send('API_ERROR: ' + error.message);
+            .send('API_ERROR(encode): ' + error.message);
         }
     };
 

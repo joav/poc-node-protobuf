62270533bcb9455bac00ef02184fb5f2804ffb1f
jim.acero.valcarcel <jim.acero.valcarcel@gmail.com>
2024-09-26 20:37:44 +0000

    Day 5 - Styles

--- public/app.mjs
+++ public/app.mjs
@@ -5,8 +5,28 @@ import { getDOMEl, setHTML } from "impure"
 import {helloRequest} from "proto"
 
 const renderMsg = flip(setHTML)
-const reqMsg = (name) => `Hello ${name}!`
-const renderReq = compose(renderMsg, reqMsg, prop("name"), helloRequest)('MyName')
+const reqMsg = (name) => `
+<div class="line system">Hello ${name}! Hello ${name}!</div>
+<div class="line system system--success">Hello ${name}!</div>
+<div class="line system system--success">Hello ${name}!</div>
+<div class="line system system--success">Hello ${name}!</div>
+<div class="line system system--success">Hello ${name}!</div>
+<div class="line system system--success">Hello ${name}!</div>
+<div class="line line--input">some input</div>
+<div class="line system system--success">Hello ${name}!</div>
+<div class="line system system--success">Hello ${name}!</div>
+<div class="line system system--success">Hello ${name}!</div>
+<div class="line system system--success">Hello ${name}!</div>
+<div class="line system system--success">Hello ${name}!</div>
+<div class="line system system--success">Hello ${name}!</div>
+<div class="line system system--success">Hello ${name}!</div>
+<div class="line system system--success">Hello ${name}!</div>
+<div class="line system system--success">Hello ${name}!</div>
+<div class="line system system--success">Hello ${name}!</div>
+<div class="line system system--success">Hello ${name}!</div>
+<div class="line system system--success">Hello ${name}!</div>
+`
+const renderReq = compose(renderMsg, reqMsg, prop("name"), helloRequest)('BLOG')
 const idSelector = (id) => `#${id}`
 
 const app = compose(renderReq, getDOMEl, idSelector)
